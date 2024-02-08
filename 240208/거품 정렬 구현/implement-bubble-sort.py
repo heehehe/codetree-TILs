da8b8a6e22b24e50b3ef918f3130a8277b7e940b
Heeseon Cheon <heeseon0315@gmail.com>
2024-02-08 22:22:31 +0900

    Title: 거품 정렬 구현, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240208/거품 정렬 구현/implement-bubble-sort.py
+++ 240208/거품 정렬 구현/implement-bubble-sort.py
@@ -4,9 +4,7 @@ def bubble_sort(array):
         is_sorted = True
         for i in range(len(array)-1):
             if array[i] > array[i+1]:
-                tmp = array[i]
-                array[i] = array[i+1]
-                array[i+1] = tmp
+                array[i], array[i+1] = array[i+1], array[i]
                 is_sorted = False
 
     return array
