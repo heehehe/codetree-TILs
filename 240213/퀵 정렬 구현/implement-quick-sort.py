ea8159e1307b42051689e4ab8f8ef1d93ecc03ba
Heeseon Cheon <heeseon0315@gmail.com>
2024-02-13 20:10:46 +0900

    Title: 퀵 정렬 구현, Time: 314ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240213/퀵 정렬 구현/implement-quick-sort.py
@@ -0,0 +1,25 @@
+def partition(array, low, high):
+    pivot = array[high]
+    i = low-1
+
+    for j in range(low, high):
+        if array[j] < pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i+1], array[high] = array[high], array[i+1]
+    return i+1
+
+def quick_sort(array, low, high):
+    if low < high:
+        index = partition(array, low, high)
+        quick_sort(array, low, index-1)
+        quick_sort(array, index+1, high)
+
+    return array
+
+length = int(input())
+array = list(map(int, input().split()))
+
+for element in quick_sort(array, 0, length-1):
+    print(element, end=" ")
